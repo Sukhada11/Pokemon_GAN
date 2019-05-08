f95961cd3984214c93b3bee443fec4fd4d254bc8
Sukhada Kulkarni <sukhadacoolkarni@gmail.com>
2019-05-08 15:29:43 +0530

    Update pokeGAN.py
    test

--- pokeGAN.py
+++ pokeGAN.py
@@ -253,25 +253,24 @@ def train():
     coord.request_stop()
     coord.join(threads)
 
-
-# def test():
-    # random_dim = 100
-    # with tf.variable_scope('input'):
-        # real_image = tf.placeholder(tf.float32, shape = [None, HEIGHT, WIDTH, CHANNEL], name='real_image')
-        # random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
-        # is_train = tf.placeholder(tf.bool, name='is_train')
+def test():
+     random_dim = 100
+     with tf.variable_scope('input'):
+         real_image = tf.placeholder(tf.float32, shape = [None, HEIGHT, WIDTH, CHANNEL], name='real_image')
+         random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
+         is_train = tf.placeholder(tf.bool, name='is_train')
     
     # # wgan
-    # fake_image = generator(random_input, random_dim, is_train)
-    # real_result = discriminator(real_image, is_train)
-    # fake_result = discriminator(fake_image, is_train, reuse=True)
-    # sess = tf.InteractiveSession()
-    # sess.run(tf.global_variables_initializer())
-    # variables_to_restore = slim.get_variables_to_restore(include=['gen'])
-    # print(variables_to_restore)
-    # saver = tf.train.Saver(variables_to_restore)
-    # ckpt = tf.train.latest_checkpoint('./model/' + version)
-    # saver.restore(sess, ckpt)
+     fake_image = generator(random_input, random_dim, is_train)
+     real_result = discriminator(real_image, is_train)
+     fake_result = discriminator(fake_image, is_train, reuse=True)
+     sess = tf.InteractiveSession()
+     sess.run(tf.global_variables_initializer())
+     variables_to_restore = slim.get_variables_to_restore(include=['gen'])
+     print(variables_to_restore)
+     saver = tf.train.Saver(variables_to_restore)
+     ckpt = tf.train.latest_checkpoint('./model/' + version)
+     saver.restore(sess, ckpt)
 
 
 if __name__ == "__main__":
